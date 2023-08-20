ed14ed5d0e9f9a2604b6ca221cbfc95c5248ae92
Demian <repositorios@3dmente.cl>
2023-08-19 21:44:21 -0400

    Cambiando urls de fetch

--- app/invitadosAdm/page.tsx
+++ app/invitadosAdm/page.tsx
@@ -1,7 +1,7 @@
 
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://fcpola.web.app"
+    ? "http://localhost:3000"
     : "http://localhost:3000";
 
 async function getData() {
--- pages/invitaciones/[invitacion].tsx
+++ pages/invitaciones/[invitacion].tsx
@@ -13,7 +13,7 @@ const EntradaInvitacion = ({ fullName }: { fullName: string }) => {
 
 async function getData() {
     try {
-        const res = await fetch(`${process.env.BASE_URL}/api/getPosts`)
+        const res = await fetch(`http://localhost:3000/api/getPosts`)
  
         if (!res.ok) {
           
